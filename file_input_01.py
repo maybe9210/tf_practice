6212281c64288c239e5f54214b19104703845d45
maybe9210 <maybe9210@gmail.com>
2019-07-24 22:39:52 +0900

    get data from filie

--- file_input_01.py
+++ file_input_01.py
@@ -1,44 +1,25 @@
 import tensorflow as tf
-import numpy as np
+
 tf.set_random_seed(777)
-xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
-x_data = xy[:, 0:-1]
-y_data = xy[:, [-1]]
 
-print(x_data, "\nx_data shape:", x_data.shape)
-print(y_data, "\ny_data shape:", y_data.shape)
+filename_queue = tf.train.string_input_producer(
+    ['data-01-test-score.csv'], shuffle=False, name="filename_queue")
 
-# x1_data = [73., 93., 89., 96., 73.]
-# x2_data = [80., 88., 91., 98., 66.]
-# x3_data = [75., 93., 90., 100., 70.]
+reader = tf.TextLineReader()
+key, value = reader.read(filename_queue)
 
-# y_data = [152., 185., 180., 196., 142.]
+record_defaults = [[0.], [0.], [0.], [0.]]
+xy = tf.decode_csv(value, record_defaults=record_defaults)
 
-# x_data = [[73., 80., 75.],
-#           [93., 88., 93.],
-#           [89., 91., 90.],
-#           [96., 98., 100.],
-#           [73., 66., 70.]]
+train_x_batch, train_y_batch = \
+    tf.train.batch([xy[0:-1], xy[-1:]], batch_size=10)
 
-# y_data = [[152.],
-#           [185.],
-#           [180.],
-#           [196.],
-#           [142.]]
-
-# x1 = tf.placeholder(tf.float32)
-# x2 = tf.placeholder(tf.float32)
-# x3 = tf.placeholder(tf.float32)
 X = tf.placeholder(tf.float32, shape=[None, 3])
 Y = tf.placeholder(tf.float32, shape=[None, 1])
 
-# w1 = tf.Variable(tf.random_normal([1]), name="weight1")
-# w2 = tf.Variable(tf.random_normal([1]), name="weight2")
-# w3 = tf.Variable(tf.random_normal([1]), name="weight3")
 W = tf.Variable(tf.random_normal([3,1]), name="weight")
 b = tf.Variable(tf.random_normal([1]), name="bias")
 
-# hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
 hypothesis = tf.matmul(X, W) + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
@@ -49,8 +30,19 @@ train = optimizer.minimize(cost)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
  
-for step in range(20):
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+for step in range(2001):
+    x_batch, y_batch = sess.run([train_x_batch, train_y_batch])
     cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
-                                    feed_dict={X: x_data, Y: y_data})
+                                    feed_dict={X: x_batch, Y: y_batch})
     if step % 10 == 0:
         print(step, "Cost: " , cost_val, "\nPrediction:\n", hy_val)
+
+coord.request_stop()
+coord.join(threads)
+
+print("Your score will be ", sess.run(hypothesis, feed_dict={X: [[100, 70, 101]]}))
+
+print("Other score will be ", sess.run(hypothesis, feed_dict={X: [[60, 70, 110], [90, 100, 80]]}))
